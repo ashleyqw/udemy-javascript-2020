2dd324f70547886bc16cfdba6a97e692e49a5642
Ashley Wang <35714960+ashleyqw@users.noreply.github.com>
2020-05-25 23:29:13 -0600

    strings and variables

---
+++ basics/strings.js
@@ -0,0 +1,12 @@
+let name = 'Michael Jordan';
+console.log(name);
+
+let firstName = 'Mariah';
+let lastName = 'Carey';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+let city = 'Paris';
+let country = 'France';
+let location = city + ', ' + country;
+console.log(location);

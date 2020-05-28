0271cc6be387589688d9fd8a58d23c95890f3366
Ashley Wang <35714960+ashleyqw@users.noreply.github.com>
2020-05-27 23:08:36 -0600

    booleans and their operators

---
+++ basics/boolean.js
@@ -0,0 +1,18 @@
+/* BOOLEAN OPERATORS
+===   equal
+!==   not equal
+<     less than
+<=    less than or equal to
+>     greater than
+>=    greater than or equal to
+*/
+
+let temp = 31;
+let isFreezing = temp === 31;
+console.log(isFreezing);
+
+let age = 21;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+console.log(isChild);
+console.log(isSenior);

de738fad61480e8ebb96c07a49c808e4737dc5fd
Ashley Wang <35714960+ashleyqw@users.noreply.github.com>
2020-05-28 22:14:50 -0600

    logic operators

--- basics/boolean.js
+++ basics/boolean.js
@@ -5,13 +5,19 @@
 <=    less than or equal to
 >     greater than
 >=    greater than or equal to
+&&    logical and
+||    logical or
 */
 
-let temp = 31;
+let temp = 91;
 let isFreezing = temp === 31;
 console.log(isFreezing);
-if (temp < 45) {
+if (temp < 40) {
   console.log('It is cold. Far too cold.');
+} else if (temp < 80) {
+  console.log('It is real pleasant. Live your life.');
+} else {
+  console.log('It is steamy out there.');
 }
 
 let age = 21;
@@ -19,3 +25,13 @@ let isChild = age <= 7;
 let isSenior = age >= 65;
 console.log(isChild);
 console.log(isSenior);
+
+let isGuestOneVegan = true;
+let isGuestTwoVegan = false;
+if (isGuestOneVegan && isGuestTwoVegan) {
+  console.log('Ahhh only vegans');
+} else if (isGuestOneVegan || isGuestTwoVegan) {
+  console.log('Need some vegan options');
+} else {
+  console.log('Give em the meats');
+}
